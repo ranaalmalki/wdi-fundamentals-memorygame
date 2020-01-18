707318f107dae18a3f70d1604ed79bd5c4031574
ranaalmalki <ranaalmalki12@gmail.com>
2020-01-18 18:19:07 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,13 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "";
-let cardFour = "";
-console.log("User flipped" +" "+ cardOne);
-console.log("User flipped" +" "+ cardTwo);
+const cards =["queen","queen","king","king"];
+const cardsInPlay =[];
+const cardOne =cards[0];
+const cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped "+""+cardOne);
+console.log("User flipped "+""+cardTwo);
+if (cardsInPlay === 2){
+alert("You found a match!");
+}else  {
+  alert("Sorry, try agine.");
+}
