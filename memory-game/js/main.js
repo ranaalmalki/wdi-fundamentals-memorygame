8f46df9655f9b315d5815bcb9cefabf56a733d3f
ranaalmalki <ranaalmalki12@gmail.com>
2020-01-19 04:50:28 +0300

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -22,29 +22,34 @@ const cards =[
 ];
 const cardsInPlay =[];
 function checkForMatch(){
+  this.setAttribute('src',cards[cardId].cardImage);
+
   if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+    alert("Sorry, try agine.");
 }
 }
-
-function flipCard(cardId){
-
+}
+function flipCard(){
+const cardId = this.getAttribute('data-id');
+this.setAttribute('src',cards[cardId].cardImage);
 console.log("User flipped "+cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-
-  if (cardsInPlay === 2){
-  alert("You found a match!");
-
-  }else  {
-    alert("Sorry, try agine.");
-
-  }
-//  checkForMatch();
+if (cardsInPlay === 2){
+checkForMatch();
+}
 
 }
-flipCard(0);
-flipCard(2);
+function createBoard(){
+  for (let i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+  cardElement.setAttribute('src',"images/back.png");
+  cardElement.setAttribute('date-id',i);
+cardElement.addEventListener('click',flipCard);
+document.getElementById('game-board').appendChild(cardElement);
+}
+}
+createBoard();
